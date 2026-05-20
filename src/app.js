5cccec1e9a52d2134b9f502d81fd3af118bdd18f
gabriel.code.works1@gmail.com <gabriel.code.works1@gmail.com>
2026-05-20 18:16:19 -0300

    V1.5

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 /*module.exports = app*/
 const express = require("express");
-const clientesRoutes = require("./routes/clientesRoutes");
-
-const express = require("express");
+const clientesRoutes = require("./src/routes/clienteRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
--- src/model/cliente.js
+++ src/model/cliente.js
@@ -1,5 +1,5 @@
 class Cliente{
-    constructor(){
+    constructor(id, nome, telefone, endereco){
         this.id = id;
         this.nome = nome;
         this.telefone = telefone;
