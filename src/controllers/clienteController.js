8ff566c8bf0d71e142696cd8d0266ee9526fc933
wagnermarques <wagnerdocri@gmail.com>
2026-05-20 22:28:46 -0300

    v1.6

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -85,8 +85,95 @@ const buscarClientePorId = async (req, res) => {
     });
   }
 };
+//PUT /
+
+const adicionarCliente = async(req, res) => {
+  try{
+    const {nome, telefone, endereco } = req.body;
+    const novo_cliente = new Cliente(
+      clientes.length + 1,
+      nome,
+      telefone,
+      endereco
+    );
+    clientes.push(novo_cliente);
+    return res.status(201).json({
+      sucesso: true,
+      mensagem: "Usuario adicionado"
+    });
+
+  }catch(error){
+    return res.status(500).json({
+      sucesso: false,
+      mensagem: "Erro ao adicionar",
+      erro: error.message
+    });
+  }
+}
+
+// PUT /Clientes/:id - Atualiza cliente pelo id:
+
+const atualizarCliente = async (req, res) => {
+  try{
+    const {id} = req.params;
+    const {nome, telefone, endereco} = req.body
+    const cliente = clientes.find((c) => c.id == id);
+    if(!cliente){
+      return res.status(404).json({
+        sucesso: false,
+        messagem: `Clientes de id $(id) não encontrado`
+      });
+    } else {
+      cliente.nome = nome;
+      cliente.telefone = telefone;
+      cliente.endereco = endereco;
+
+      return res.status(200).json({
+        sucesso: true,
+        mensagem: "Cliente atualizado"
+      })
+    }
+  }catch(error){
+    return res.status(500).json({
+      sucesso: false,
+      mensagem: "Erro ao atualizar cliente",
+      erro: error.message
+    })
+  }
+}
+
+// DELETE /clientes,js/:id - remove um cliente pelo id
+
+const deletarCliente = async(req, res) => {
+  try{
+    const {id} = req.params;
+    const index = clientes.findIndex((c) => c.id == id);
+
+    if(index === -1){
+      return res.status(404).json({
+        sucesso: false,
+        mensagem: `Cliente de ${id} não encontrado`
+      })
+    } else {
+      clientes.splice(index, 1);
+      return res.status(200).json({
+        sucesso: true,
+        mensagem: `cliente com ${id} removido com sucesso`
+      });
+    }
+  }catch(error) {
+    return res.status(500).json({
+      sucesso: false,
+      mensagem: "Erro ao remover cliente",
+      erro: error.message
+    })
+  }
+}
 
 module.exports = {
   listarClientes,
   buscarClientePorId,
+  adicionarCliente,
+  atualizarCliente,
+  deletarCliente
 };
